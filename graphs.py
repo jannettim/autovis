bdcaf9c041fce6aa81b9c5ec681dda8d9c067f2d
jannettim <jannettim@gmail.com>
2017-08-09 21:42:04 -0400

    Migrated from flask to bokeh server so I don't have to write javascript

--- graphs.py
+++ graphs.py
@@ -1,128 +1,154 @@
 import seaborn
 import numpy as np
-from bokeh.plotting import figure
-from bokeh.embed import components
-from bokeh.resources import INLINE
-import bokeh.palettes
+from bokeh.plotting import figure, curdoc
 from bokeh.models import CustomJS, Slider, ColumnDataSource, Palette, Select, ColorMapper
-import pandas as pd
+from bokeh.client import push_session, ClientSession
 from matplotlib import pyplot
+from bokeh.layouts import layout, row
+from math import floor, ceil
+
+from functools import partial
+import pandas as pd
 
 
-def palette_maps(n_colors):
+class GraphPlot:
+
+    def __init__(self, x, y, **kwargs):
+
+        self.plot_width = kwargs["plot_width"]
+        self.plot_height = kwargs["plot_height"]
+        # self.doc = kwargs["doc"]
+        # self.session = kwargs["session"]
+
+        if isinstance(y, pd.Series) and isinstance(x, pd.DataFrame):
+            self.num_colors = len(x.columns)
+            self.palettes = self.palette_maps(self.num_colors)
+            self.palette = self.palettes[kwargs["palette"]]
+
+            self.ys = y.repeat(self.num_colors).values
+            self.xs = x.values.flatten()
+            self.group = floor(len(self.xs)/len(self.palette))
+            self.colors = self.palette * self.group
+            print(self.colors)
+
+        elif isinstance(y, pd.DataFrame) and isinstance(x, pd.DataFrame):
+
+            self.num_colors = len(x.columns)
+            self.palettes = self.palette_maps(self.num_colors)
+            self.palette = self.palettes[kwargs["palette"]]
+
+            if self.num_colors != len(y.columns):
+
+                raise ValueError("If y values are dataframe, number of y columns needs to equal number of x columns. "
+                                 "If y is the same value repeated for each x then pass y as a series")
+
+            self.ys = y.values.flatten()
+            self.xs = x.values.flatten()
+            self.group = floor(len(self.xs)/len(self.palette))
+            self.colors = self.palette * self.group
+
+        elif isinstance(y, pd.DataFrame) and isinstance(x, pd.Series):
+
+            self.num_colors = len(y.columns)
+            self.palettes = self.palette_maps(self.num_colors)
+            self.palette = self.palettes[kwargs["palette"]]
+
+            self.xs = x.repeat(self.num_colors).values
+            self.ys = y.values.flatten()
+
+            self.group = floor(len(self.ys)/len(self.palette))
+            self.colors = self.palette * self.group
+
+        elif isinstance(y, pd.Series) and isinstance(x, pd.Series):
+
+            self.palettes = self.palette_maps(1)
+            self.palette = self.palettes[kwargs["palette"]]
+            self.num_colors = 1
+
+            self.xs = x.tolist()
+            self.ys = y.tolist()
+            self.group = self.palette * len(self.ys)
+            self.colors = self.palette * self.group
+
+        else:
+
+            raise ValueError
+
+        self.source = ColumnDataSource(data=dict(x=self.xs, y=self.ys, color=self.colors))
+        self.col_source = ColumnDataSource(data=self.palettes)
+
+        self.graph = None
+
+    def palette_maps(self, n_colors):
+
+        palette_map = {}
+
+        for n in pyplot.colormaps():
+
+            if n == "jet":
+
+                continue
+
+            palette_map.update({n: seaborn.color_palette(n, n_colors).as_hex()})
+
+        return palette_map
+
+    def change_palette(self, attr, old, new):
+        self.palette = self.palettes[new]
+
+        self.colors = self.palette * self.group
+        curdoc().add_next_tick_callback(partial(self.update, x=self.xs, y=self.ys, c=self.colors))
+
+    def change_dot_size(self, attr, old, new):
+
+        self.graph.glyph.size = new
+        curdoc().add_next_tick_callback(partial(self.update, x=self.xs, y=self.ys, c=self.colors))
+
+    def update(self, x, y, c):
+
+        self.source.data["color"] = []
+        self.source.stream(dict(x=x, y=y, color=c))
+
+    def plot_scatter(self):
+
+        p = figure(plot_width=self.plot_width, plot_height=self.plot_height)
+
+        self.graph = p.scatter('x', 'y', color="color", source=self.source)
+
+        select_pal = Select(options=[c for c in pyplot.colormaps() if c != "jet"])
+
+        dot_size_slider = Slider(start=1, end=100, value=1, step=1, title="Dot Size")
+        dot_size_slider.on_change("value", self.change_dot_size)
+        select_pal.on_change("value", self.change_palette)
+
+        app_layout = layout([[select_pal],
+                             [p],
+                             [dot_size_slider]])
+
+        # curdoc().add_root(app_layout)
+        #
+        return app_layout
 
 
-    palette_map = {}
-
-    for n in pyplot.colormaps():
-
-        if n == "jet":
-
-            continue
-
-        palette_map.update({n: seaborn.color_palette(n, n_colors).as_hex()})
-
-    return palette_map
+# def main(doc):
+#
+#     df = pd.DataFrame(np.random.randn(10, 4), columns=["x1", "x2", "y1", "y2"])
+#     gp = GraphPlot(df[["x1", "x2"]], df[["y1", "y2"]], palette="Accent", plot_width=600, plot_height=600)
+#
+#     app_layout = gp.plot_scatter()
+#
+#     doc.add_root(app_layout)
 
 
-def plot_scatter(x, y, **kwargs):
+df = pd.DataFrame(np.random.randn(10, 4), columns=["x1", "x2", "y1", "y2"])
+gp = GraphPlot(df[["x1", "x2"]], df[["y1", "y2"]], palette="Accent", plot_width=600, plot_height=600)# , doc=doc,
+               # session=session)
 
-    if isinstance(y, pd.Series) and isinstance(x, pd.DataFrame):
-        num_xs = len(x.columns)
-        palettes = palette_maps(num_xs)
-        palette = palettes[kwargs["palette"]]
+app_layout = gp.plot_scatter()
 
-        ys = y.repeat(num_xs).values
-        xs = x.values.flatten()
-        colors = palette * (len(xs)/len(palette))
+doc = curdoc()
+doc.add_root(app_layout)
 
-    elif isinstance(y, pd.DataFrame) and isinstance(x, pd.DataFrame):
-
-        num_xs = len(x.columns)
-        palettes = palette_maps(num_xs)
-        palette = palettes[kwargs["palette"]]
-
-        if num_xs != len(y.columns):
-
-            raise ValueError("If y values are dataframe, number of y columns needs to equal number of x columns. "
-                             "If y is the same value repeated for each x then pass y as a series")
-
-        ys = y.values.flatten()
-        xs = x.values.flatten()
-        colors = palette * (len(xs)/len(palette))
-
-    elif isinstance(y, pd.DataFrame) and isinstance(x, pd.Series):
-
-        num_ys = len(y.columns)
-        palettes = palette_maps(num_ys)
-        palette = palettes[kwargs["palette"]]
-
-        xs = x.repeat(num_ys).values
-        ys = y.values.flatten()
-
-        colors = palette * (len(ys)/len(palette))
-
-    elif isinstance(y, pd.Series) and isinstance(x, pd.Series):
-
-        palettes = palette_maps(1)
-        palette = palettes[kwargs["palette"]]
-
-        xs = x.tolist()
-        ys = y.tolist()
-        colors = palette * len(ys)
-
-    else:
-
-        raise ValueError
-
-    p = figure(plot_width=kwargs["plot_width"], plot_height=kwargs["plot_height"])
-    source = ColumnDataSource(data=dict(x=xs, y=ys, color=colors))
-    col_source = ColumnDataSource(data=palettes)
-    sc = p.scatter('x', 'y', color="color", source=source)
-
-    def callback(source=source, sc=sc, window=None):
-
-        f = cb_obj.value
-        sc.glyph.size = f
-        source.change.emit()
-
-    def callback_pal(source=source, col=col_source, window=None):
-
-        data = source.data
-        new_pal = col.data[cb_obj.value]
-
-        pal_map = {}
-        pal = []
-        for d in data["color"]:
-
-            if d not in pal:
-
-                pal.append(d)
-
-        for c in range(len(pal)):
-
-            pal_map[pal[c]] = new_pal[c]
-
-        for i in range(len(data["color"])):
-
-            new_col = pal_map[data["color"][i]]
-            data["color"][i] = new_col
-
-        source.change.emit()
-
-    dot_size_slider = Slider(start=0, end=100, value=1, step=1, title="Dot Size",
-                             callback=CustomJS.from_py_func(callback))
-    select_pal = Select(options=[c for c in pyplot.colormaps() if c != "jet"],
-                        callback=CustomJS.from_py_func(callback_pal))
-    js_resources = INLINE.render_js()
-    css_resources = INLINE.render_css()
-
-    script, div = components({"p": p, "dot_size_slider": dot_size_slider, "pal": select_pal}, INLINE)
-
-    return script, div, js_resources, css_resources
-
-if __name__ == '__main__':
-
-    df = pd.DataFrame(np.random.randn(10, 4), columns=["x1", "x2", "y1", "y2"])
-
-    plot_scatter(df[["x1", "x2"]], df[["y1", "y2"]], palette="Accent", plot_width=600, plot_height=600)
+# session = push_session(doc)
+# session.show()
+# session.loop_until_closed()
