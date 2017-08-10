8dc817668bbfb3072e5095aad50dbe3ded82392a
jannettim <jannettim@gmail.com>
2017-08-09 22:35:08 -0400

    series fix

--- graphs.py
+++ graphs.py
@@ -17,22 +17,35 @@ class GraphPlot:
 
         self.plot_width = kwargs["plot_width"]
         self.plot_height = kwargs["plot_height"]
-        # self.doc = kwargs["doc"]
-        # self.session = kwargs["session"]
+
+        try:
+
+            self.group = kwargs["group"]
+
+        except KeyError:
+
+            self.group = None
 
         if isinstance(y, pd.Series) and isinstance(x, pd.DataFrame):
+
+            self.series = False
+
+            # if not self.group:
             self.num_colors = len(x.columns)
+            # else:
+            #     self.num_colors = len(self.group)
+
             self.palettes = self.palette_maps(self.num_colors)
             self.palette = self.palettes[kwargs["palette"]]
 
             self.ys = y.repeat(self.num_colors).values
             self.xs = x.values.flatten()
-            self.group = floor(len(self.xs)/len(self.palette))
-            self.colors = self.palette * self.group
-            print(self.colors)
+            self.num_mod = floor(len(self.xs) / len(self.palette))
+            self.colors = self.palette * self.num_mod
 
         elif isinstance(y, pd.DataFrame) and isinstance(x, pd.DataFrame):
 
+            self.series = False
             self.num_colors = len(x.columns)
             self.palettes = self.palette_maps(self.num_colors)
             self.palette = self.palettes[kwargs["palette"]]
@@ -44,11 +57,12 @@ class GraphPlot:
 
             self.ys = y.values.flatten()
             self.xs = x.values.flatten()
-            self.group = floor(len(self.xs)/len(self.palette))
-            self.colors = self.palette * self.group
+            self.num_mod = floor(len(self.xs) / len(self.palette))
+            self.colors = self.palette * self.num_mod
 
         elif isinstance(y, pd.DataFrame) and isinstance(x, pd.Series):
 
+            self.series = False
             self.num_colors = len(y.columns)
             self.palettes = self.palette_maps(self.num_colors)
             self.palette = self.palettes[kwargs["palette"]]
@@ -56,19 +70,21 @@ class GraphPlot:
             self.xs = x.repeat(self.num_colors).values
             self.ys = y.values.flatten()
 
-            self.group = floor(len(self.ys)/len(self.palette))
-            self.colors = self.palette * self.group
+            self.num_mod = floor(len(self.ys) / len(self.palette))
+            self.colors = self.palette * self.num_mod
 
         elif isinstance(y, pd.Series) and isinstance(x, pd.Series):
 
+            self.series = True
             self.palettes = self.palette_maps(1)
             self.palette = self.palettes[kwargs["palette"]]
             self.num_colors = 1
 
             self.xs = x.tolist()
             self.ys = y.tolist()
-            self.group = self.palette * len(self.ys)
-            self.colors = self.palette * self.group
+            self.num_mod = len(self.palette) * len(self.ys)
+            self.colors = self.palette * self.num_mod
+            print(self.colors)
 
         else:
 
@@ -96,7 +112,8 @@ class GraphPlot:
     def change_palette(self, attr, old, new):
         self.palette = self.palettes[new]
 
-        self.colors = self.palette * self.group
+        self.colors = self.palette * self.num_mod
+        print(self.colors)
         curdoc().add_next_tick_callback(partial(self.update, x=self.xs, y=self.ys, c=self.colors))
 
     def change_dot_size(self, attr, old, new):
@@ -106,7 +123,10 @@ class GraphPlot:
 
     def update(self, x, y, c):
 
+        # if not self.series:
         self.source.data["color"] = []
+        self.source.data["x"] = []
+        self.source.data["y"] = []
         self.source.stream(dict(x=x, y=y, color=c))
 
     def plot_scatter(self):
@@ -124,31 +144,13 @@ class GraphPlot:
         app_layout = layout([[select_pal],
                              [p],
                              [dot_size_slider]])
-
-        # curdoc().add_root(app_layout)
-        #
         return app_layout
 
 
-# def main(doc):
-#
-#     df = pd.DataFrame(np.random.randn(10, 4), columns=["x1", "x2", "y1", "y2"])
-#     gp = GraphPlot(df[["x1", "x2"]], df[["y1", "y2"]], palette="Accent", plot_width=600, plot_height=600)
-#
-#     app_layout = gp.plot_scatter()
-#
-#     doc.add_root(app_layout)
-
-
 df = pd.DataFrame(np.random.randn(10, 4), columns=["x1", "x2", "y1", "y2"])
-gp = GraphPlot(df[["x1", "x2"]], df[["y1", "y2"]], palette="Accent", plot_width=600, plot_height=600)# , doc=doc,
-               # session=session)
+gp = GraphPlot(df[["x1", "x2"]], df[["y1", "y2"]], palette="Accent", plot_width=600, plot_height=600)
 
 app_layout = gp.plot_scatter()
 
 doc = curdoc()
 doc.add_root(app_layout)
-
-# session = push_session(doc)
-# session.show()
-# session.loop_until_closed()
